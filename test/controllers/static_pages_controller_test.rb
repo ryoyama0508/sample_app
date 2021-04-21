29ac8939412272789be94700401ea5b8ee8647f4
Ryo Yamamoto <ryo.dr.dre0508@gmail.com>
2021-04-21 15:42:27 +0900

    finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
+  test "should get root" do
+    get static_pages_home_url
+    assert_response :success
+  end
+
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
